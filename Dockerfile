6648ad43604cf70c253d8bb84441bd1219e1245e
stash96 <stash96@rtm.ai>
2021-11-03 10:48:58 +0900

    update day3

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,8 @@ COPY requirements.txt .
 
 RUN mkdir -p /model
 
-RUN apt-get update && apt-get upgrade -y
-
 RUN pip install --trusted-host pypi.python.org -r requirements.txt
 
-EXPOSE 8000
+EXPOSE 80
 
-CMD ["uvicorn", "--host=0.0.0.0", "--port=8000", "main:app"]
+CMD ["uvicorn", "--host=0.0.0.0", "--port=80", "main:app"]
--- app/main.py
+++ app/main.py
@@ -1,43 +1,22 @@
 import uvicorn
 from typing import Dict, Optional
 from fastapi import FastAPI
-from LOT_AD_production.inference import load_pickle
-from LOT_AD_production.inference import get_output
+# from LOT_AD_production.inference import load_pickle
+# from LOT_AD_production.inference import get_output
 
 app = FastAPI()
 
-@app.get("/load")
-def load():
-    global model
-    model = load_pickle("/model/latest.pkl")
-    return {"모델":model}
+@app.get("/")
+def read_root():
+    return {"Hello":"World?!"}
 
-@app.get("/classification")
-def classification(q: Optional[str] = None):
-    output = get_output(model, {})
+# @app.get("/load")
+# def load():
+#     global model
+#     model = load_pickle("/model/latest.pkl")
+#     return {"모델":model}
 
-    return output
-# def load_model(model_path: str):
-#     # 경로로 모델 오브젝트 return
-#     model = (model_path)
-#     # model_path == "/model/latest.pkl"
-
-#     return model
-
-# def classification(input_tuple):
-#     model_ = input_tuple[0]
-    
-#     device = input_tuple[1]
-#     device_name = device[0]
-#     cal_dict = device[1]
-#     cal_name = cal_dict[0]
-#     cal_value = cal_dict[1]
-
-#     cal_name_list = input_tuple[2]
-    
-#     output_tuple = (str, {"causing_component_name": {"":""}})
-
-#     return output_tuple
-
-if __name__ == '__main__':
-    uvicorn.run(app='main:app', host="127.0.0.1", port=8000, reload=True, debug=True)
+# @app.get("/classification")
+# def classification(q: Optional[str] = None):
+#     output = get_output(model, {"측정명A1":0.0592}, ["측정명1", "측정명2", "측정명3"])
+#     return output
